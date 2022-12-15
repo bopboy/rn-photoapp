1779c8cc2b5df3a481d44ad0f225e144230d3602
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-15 19:37:08 +0900

    입력 컴포넌트 스타일링

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { forwardRef, useState } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { PRIMARY, GRAY } from '../colors';
 
 export const InputTypes = {
     EMAIL: 'EMAIL',
@@ -30,7 +31,7 @@ export const ReturnKeyType = {
     NEXT: 'next',
 };
 
-const Input = forwardRef(({ inputType, ...props }, ref) => {
+const Input = forwardRef(({ inputType, styles, ...props }, ref) => {
     const {
         title,
         placeholder,
@@ -39,15 +40,36 @@ const Input = forwardRef(({ inputType, ...props }, ref) => {
         iconName: { active, inactive },
     } = InputTypeProps[inputType];
     const [isFocused, setIsFocused] = useState(false);
+    const { value } = props;
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{title}</Text>
+        <View style={[defaultStyles.container, styles?.container]}>
+            <Text
+                style={[
+                    defaultStyles.title,
+                    { color: value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK },
+                ]}
+            >
+                {title}
+            </Text>
             <View style={{}}>
                 <TextInput
                     ref={ref}
                     {...props}
-                    style={[styles.input]}
+                    style={[
+                        defaultStyles.input,
+                        {
+                            borderColor:
+                                value || isFocused
+                                    ? PRIMARY.DEFAULT
+                                    : GRAY.DARK,
+                            color:
+                                value || isFocused
+                                    ? PRIMARY.DEFAULT
+                                    : GRAY.DARK,
+                        },
+                        styles?.input,
+                    ]}
                     placeholder={placeholder}
                     keyboardType={keyboardType}
                     secureTextEntry={secureTextEntry}
@@ -55,11 +77,11 @@ const Input = forwardRef(({ inputType, ...props }, ref) => {
                         setIsFocused(false);
                     }}
                 />
-                <View style={styles.icon}>
+                <View style={[defaultStyles.icon, styles?.icon]}>
                     <MaterialCommunityIcons
                         name={isFocused ? active : inactive}
                         size={24}
-                        color="black"
+                        color={value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK}
                     />
                 </View>
             </View>
@@ -70,9 +92,11 @@ Input.displayName = 'Input';
 
 Input.propTypes = {
     inputType: PropTypes.oneOf(Object.values(InputTypes)),
+    value: PropTypes.string,
+    styles: PropTypes.object,
 };
 
-const styles = StyleSheet.create({
+const defaultStyles = StyleSheet.create({
     container: {
         width: '100%',
     },
@@ -84,6 +108,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         height: 42,
         paddingHorizontal: 10,
+        paddingLeft: 40,
     },
     icon: {
         position: 'absolute',
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,8 +2,12 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { AuthRoutes } from '../navigations/routes';
 import Input, { InputTypes } from '../components/Input';
+import { useState } from 'react';
 
 const SignInScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>SignInScreen</Text>
@@ -11,8 +15,20 @@ const SignInScreen = ({ navigation }) => {
                 title="signup"
                 onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
             />
-            <Input inputType={InputTypes.EMAIL} />
-            <Input inputType={InputTypes.PASSWORD} />
+            <Input
+                inputType={InputTypes.EMAIL}
+                value={email}
+                onChangeText={(text) => setEmail(text.trim())}
+                styles={{
+                    container: { paddingHorizontal: 20, marginBottom: 20 },
+                }}
+            />
+            <Input
+                inputType={InputTypes.PASSWORD}
+                value={password}
+                onChangeText={(text) => setPassword(text.trim())}
+                styles={inputStyles}
+            />
         </View>
     );
 };
@@ -21,6 +37,9 @@ SignInScreen.propTypes = {
     navigation: PropTypes.object,
 };
 
+const inputStyles = StyleSheet.create({
+    container: { paddingHorizontal: 20, marginBottom: 20 },
+});
 const styles = StyleSheet.create({
     container: {
         flex: 1,
