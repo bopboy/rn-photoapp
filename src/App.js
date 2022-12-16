597fc9144cfff6e48092e0ddf72b544b99bd8819
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-16 09:11:05 +0900

    배경 화면 이미지 캐싱
    (이 부분은 개선이 잘 느껴지지 않는다)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import Navigation from './navigations';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
+import { Asset } from 'expo-asset';
 
 const App = () => {
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                await SplashScreen.preventAutoHideAsync();
+                await Asset.fromModule(
+                    // eslint-disable-next-line no-undef
+                    require('../assets/cover.png').downloadAsync()
+                );
+            } catch (e) {
+                console.log(e);
+            } finally {
+                // setTimeout(() => {
+                setIsReady(true);
+                // }, 1000);
+            }
+        })();
+    }, []);
+
+    const onReady = async () => {
+        if (isReady) await SplashScreen.hideAsync();
+    };
+
+    if (!isReady) return null;
+
     return (
-        <>
+        <View style={{ flex: 1 }} onLayout={onReady}>
             <StatusBar style={'dark'} />
             <Navigation />
-        </>
+        </View>
     );
 };
 
