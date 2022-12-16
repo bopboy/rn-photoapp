8681e14c6c509512c02ca3f9fb5461d2da47bcc3
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-16 17:21:14 +0900

    탭 네비게이터를 스택 첫 화면으로 사용

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -6,7 +6,7 @@ import AuthStack from './AuthStack';
 import * as SplashScreen from 'expo-splash-screen';
 import { Asset } from 'expo-asset';
 import { initFirebase } from '../api/firebase';
-import ContentTab from './ContentTab';
+import MainStack from './MainStack';
 
 const Navigation = () => {
     const [user, setUser] = useUserState();
@@ -43,7 +43,7 @@ const Navigation = () => {
 
     return (
         <NavigationContainer onReady={onReady}>
-            {user.uid ? <ContentTab /> : <AuthStack />}
+            {user.uid ? <MainStack /> : <AuthStack />}
         </NavigationContainer>
     );
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { WHITE } from '../colors';
+import { MainRoutes } from '../navigations/routes';
 // import PropTypes from 'prop-types';
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
     return (
         <View style={styles.container}>
             <Text style={styles.title}>HomeScreen</Text>
+            <Button
+                title={'select'}
+                onPress={() => navigation.navigate(MainRoutes.SELECT_PHOTOS)}
+            />
         </View>
     );
 };
