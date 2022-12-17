112cf4c2891def6189bb27684c3fa3d723577667
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-17 15:45:57 +0900

    포토 라이브러리 접근 권한 요청

--- src/screens/ImagePickerScreen.js
+++ src/screens/ImagePickerScreen.js
@@ -1,18 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 // import PropTypes from 'prop-types';
 import { useNavigation } from '@react-navigation/native';
 import HeaderRight from '../components/HeaderRight';
-import { useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
+import * as MediaLibrary from 'expo-media-library';
 
 const ImagePickerScreen = () => {
     const navigation = useNavigation();
+    const [status, requestPermission] = MediaLibrary.usePermissions();
+    console.log(status);
 
+    useEffect(() => {
+        (async () => {
+            const { granted } = await requestPermission();
+            if (!granted) {
+                Alert.alert('사진 접근 권한', '사진 접근 권한 필요해', [
+                    {
+                        text: '확인',
+                        onPress: () =>
+                            navigation.canGoBack() && navigation.goBack(),
+                    },
+                ]);
+            }
+        })();
+    }, []);
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => <HeaderRight onPress={() => {}} />,
         });
     }, []);
-    
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>ImagePickerScreen</Text>
